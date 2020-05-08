43a45e5869505f7038665e0bfbc78d1f3acd2818
Bruna Cardoso <bruna.carolinepc@gmail.com>
2020-05-08 20:09:37 -0300

    Leitura dos dados

--- src/application/projetobar.java
+++ src/application/projetobar.java
@@ -1,10 +1,29 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class projetobar {
 
 	public static void main(String[] args) {
 		
-
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int cerveja, refri, espeto;
+		char sexo;
+		
+		System.out.println("Sexo: ");
+		sexo = sc.next().charAt(0);
+		
+		System.out.println("Quantidade de cervejas: ");
+		cerveja = sc.nextInt();
+		
+		System.out.println("Quantidade de refrigerantes: ");
+		refri = sc.nextInt();
+		
+		System.out.println("Quantidade de espetinhos: ");
+		espeto = sc.nextInt();
 	}
 
 }
